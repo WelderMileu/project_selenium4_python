694c8b909e457ee8d3ce679484cda4e783936689
WelderMileu <45273258+WelderMileu@users.noreply.github.com>
2024-05-04 01:40:19 +0000

    Update main.py
    add new function

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ from time import sleep
 
 class Simple:
     def __init__(self):
-        self.Service = FirefoxService(GeckoDriverManager().install())
-        self.Options = webdriver.FirefoxOptions()
-        self.Driver  = webdriver.Firefox(service=self.Service, options=self.Options)
-        self.Domain  = 'https://www.google.com'
+        self.service = FirefoxService(GeckoDriverManager().install())
+        self.options = webdriver.FirefoxOptions()
+        self.private = self.options.add_argument('--private-window')
+        self.driver  = webdriver.Firefox(service=self.service, options=self.options)
+        self.domain  = ''
 
     def init(self):
         try: 
-            self.Options.add_argument('--private-window') 
-            self.Driver.get(self.Domain)
+            self.domain = 'https://www.google.com'
+            self.driver.get(self.domain)
 
             sleep(3)
             id_elem = '//*[@id="APjFqb"]'
 
-            elem_search = self.Driver.find_element(By.XPATH, id_elem)
+            elem_search = self.driver.find_element(By.XPATH, id_elem)
             elem_search.clear()
             elem_search.send_keys("youtube")
             elem_search.send_keys(Keys.ENTER)
@@ -30,6 +31,46 @@ class Simple:
         except Exception as e:
             print(e)
 
+    def instagram_stories(self):
+        try:
+            from base64 import b64decode
+
+            path_url    = ""
+            prof_hash   = ""
+            prof_decode = b64decode(prof_hash).decode()
+
+            self.domain = path_url
+            self.driver.get(self.domain)
+
+            xpath = '/html/body/div[1]/header/div[1]/div/form/div/input'
+            ipt_search = self.driver.find_element(By.XPATH, xpath)
+
+            ipt_search.send_keys(prof_decode)
+            ipt_search.send_keys(Keys.ENTER)
+
+            sleep(3)
+            original_tab = self.driver.current_window_handle
+            self.driver.switch_to.window(original_tab)
+
+            button_dowload_xpath = '/html/body/div[1]/div[1]/div/div/ul[2]'
+            all_button_download  = self.driver.find_element(By.XPATH, button_dowload_xpath)
+            
+            link = all_button_download.find_elements(By.TAG_NAME, 'a')
+
+            for x in link:
+                import wget
+                from uuid import uuid4
+
+                url = x.get_attribute('href')
+                wget.download(url)
+
+        except Exception as e:
+            print(e)
+
+    def main(self):
+        Simple.instagram_stories()
+
+
 if __name__ == '__main__':
     Simple = Simple()
-    Simple.init()
+    Simple.main()
